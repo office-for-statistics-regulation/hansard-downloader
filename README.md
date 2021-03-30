c0440c84466f6b5ad5ac4ff5af543e647c9dd899
Michael Hodge <michael.hodge@ons.gov.uk>
2021-03-30 09:51:36 +0100

    Coding standards

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Uses BeautifulSoup and requests packages to download data from [Hansard](https://hansard.parliament.uk/) for the previous days debates. Saves these into a dataframe.
 
 TODO:
-- [ ] Document code better
+- [x] Document code better
 - [ ] Document clustering into OSR domains
 - [ ] Write cloud version
 - [ ] Save to BigQuery
--- local/functions/harnsard-downloader/src/main.py
+++ local/functions/harnsard-downloader/src/main.py
@@ -1,13 +1,21 @@
-import requests
 from datetime import datetime, timedelta
+
+import requests
 from bs4 import BeautifulSoup as Soup
 import pandas as pd
 
 
-def scrape_hansard():
-	yesterdays_date = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
-	# getting the 'count' of the number of overall publications to determine number of pages
-	url = 'https://hansard.parliament.uk/commons/' + yesterdays_date
+def scrape_hansard(date):
+	"""Scrapes Hansard publications for a certain date.
+
+	Keyword arguments:
+	date -- the date (in YYYY-MM-DD format)
+
+	Output:
+	A dataframe for the scraped data, saved to same folder as script
+	"""
+
+	url = 'https://hansard.parliament.uk/commons/' + date
 	response = requests.get(url, timeout=5)
 	page_content = Soup(response.content, "html.parser")
 	folders = page_content.find_all("div", attrs={"class": "card-folder"})
@@ -23,23 +31,26 @@ def scrape_hansard():
 	stats_lst = []
 
 	for folder in folders:
+		# Searching just for H2 text which denotes the folder name of the publication
 		if len(folder.find_all('h2')) > 0:
 			folder_name = folder.text.split("\n")[2]
 			folder_desc = folder.text.split("\n")[3]
 			sub_folder_name = ''
 
 		else:
+			# Otherwise it's a subfolder
 			sub_folder_name = folder.text.split("\n")[2]
-
 
 		cards = folder.find_all("a", attrs={"class": "card card-section"})
 		for card in cards:
+			# Gets each card in each folder, finds it's URL link
 			link = 'https://hansard.parliament.uk/' + card.attrs.get('href')
 			folder_name_lst.append(folder_name)
 			folder_desc_lst.append(folder_desc)
 			sub_folder_name_lst.append(sub_folder_name)
 			link_lst.append(link)
 
+			# Goes to the relevant URL
 			response = requests.get(link, timeout=5)
 			page_content = Soup(response.content, "html.parser")
 			content = page_content.find("div", attrs={"class": "col-lg-9 primary-content"})
@@ -47,6 +58,7 @@ def scrape_hansard():
 
 			text_out = ''
 			for para in paras:
+				# Collects all relevant text for the page
 				if len(para.text) > 5:
 					text_out = text_out + ' ' + para.text
 			text_lst.append(text_out)
@@ -58,11 +70,16 @@ def scrape_hansard():
 
 	df['date_collected'] = datetime.now().strftime('%d/%m/%Y')
 
-	df.to_csv("latest_run.csv")
+	if len(df) == 0:
+		print(f"No records found from Hansard for {date}")
+		return
 
-	print(f"Found {len(df)} records from Hansard")
-	return 'Complete'
+	else:
+		df.to_csv("latest_run.csv")
+		print(f"Found {len(df)} records from Hansard for {date}")
+		return 'Complete'
 
 
 if __name__ == '__main__':
-	scrape_hansard()
+	date = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d')  # Yesterdays date
+	scrape_hansard(date)
