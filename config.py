b4a8723119391c719b023af3e8108892d4a0ce38
Michael Hodge <michael.hodge@ons.gov.uk>
2021-06-25 16:40:20 +0100

    Statistics scraper added v1

---
+++ config.py
@@ -0,0 +1,5 @@
+# List the search terms here you think would be in a paragraph that has statistics in, e.g.
+# 500 million, 7.6 percent, half of all people, etc
+# Note, at the moment only sentences with numerical values are pulled out, so half of all people would not be
+# included unless a numerical statistics also exists in the same paragraph
+stats_search_terms = ['million','thousand','hundred','%','percent','half','quarter','double']
--- hansard-downloader.py
+++ hansard-downloader.py
@@ -5,6 +5,8 @@ import requests
 from bs4 import BeautifulSoup as Soup
 import pandas as pd
 
+import config
+
 
 def scrape_hansard(date):
 	"""Scrapes Hansard publications for a certain date.
@@ -30,6 +32,7 @@ def scrape_hansard(date):
 	card_lst = []
 	link_lst = []
 	text_lst = []
+	stats_cited = []
 
 	for folder in folders:
 		# Searching just for H2 text which denotes the folder name of the publication
@@ -44,43 +47,65 @@ def scrape_hansard(date):
 
 		cards = folder.find_all("a", attrs={"class": "card card-section"})
 		for card in cards:
-			# Gets name of the card
-			card_lst.append(card.text.split('\r\n')[1].strip())
 
-			# Gets each card in each folder, finds it's URL link
-			link = 'https://hansard.parliament.uk/' + card.attrs.get('href')
-			folder_name_lst.append(folder_name)
-			folder_desc_lst.append(folder_desc)
-			sub_folder_name_lst.append(sub_folder_name)
-			link_lst.append(link)
+			if 'View all' not in card.text:
+				# Gets name of the card
+				card_lst.append(card.text.split('\r\n')[1].strip())
 
-			# Goes to the relevant URL
-			response = requests.get(link, timeout=5)
-			page_content = Soup(response.content, "html.parser")
-			content = page_content.find("div", attrs={"class": "col-lg-9 primary-content"})
-			paras = content.find_all("p")
+				# Gets each card in each folder, finds it's URL link
+				link = 'https://hansard.parliament.uk/' + card.attrs.get('href')
+				folder_name_lst.append(folder_name)
+				folder_desc_lst.append(folder_desc)
+				sub_folder_name_lst.append(sub_folder_name)
+				link_lst.append(link)
 
-			text_out = ''
-			for para in paras:
-				# Collects all relevant text for the page
-				if len(para.text) > 5:
-					text_out = text_out + ' ' + para.text
-			text_lst.append(text_out)
+				# Goes to the relevant URL and downloads the txt file
+				response = requests.get(link, timeout=5)
+				page_content = Soup(response.content, "html.parser")
+				download_url = 'https://hansard.parliament.uk' + page_content.find("a", attrs={"class": "icon-link"}).attrs.get('href')
+				text = requests.get(download_url).content.decode("utf-8")
+
+				# Searches through the text for paragraph where numerical values exist
+				paras = text.split('\r')
+				stats_cited_tmp = []
+				for para in paras:
+					if any(word in para for word in config.stats_search_terms):
+						words = para.split(' ')
+						for word in words:
+							if word.isnumeric():
+								stats_cited_tmp.append(para.strip('\n'))
+
+				text_lst.append(text)
+				stats_cited.append(stats_cited_tmp)
 
 	df = pd.DataFrame(list(zip(folder_name_lst, folder_desc_lst, sub_folder_name_lst, card_lst, link_lst,
-							   text_lst)),
-					  columns=['Folder', 'Description', 'Subfolder', 'Card', 'Link', 'Text'])
+							   text_lst, stats_cited)),
+					  columns=['Folder', 'Description', 'Subfolder', 'Card', 'Link', 'Text', 'Stats'])
 
 	df['date_collected'] = datetime.now().strftime('%d/%m/%Y')
+
+	# Keep a dataframe with the raw text in
+	df_text = df.drop('Stats', 1)
+
+	# Make a long dataframe with each paragraph in that has a statistic referenced
+	df_stats = df.drop('Text', 1)
+	df_stats = df_stats[~df_stats.Stats.str.len().eq(0)] # Drop empty Stats rows
+	df_stats = df_stats.explode('Stats')
+	df_stats = df_stats.drop_duplicates() # Drop duplicates because some sentences may have more than one statistic sourced
+
 
 	if len(df) == 0:
 		print(f"No records found from Hansard for {date}")
 		return
 
 	else:
-		df = df[df.Card != ''] #Drop empty Card column entries as these will be repeats of Cards below
-		df.to_csv(f"output/hansard-{date}.csv", index=False)
-		print(f"Found {len(df)} records from Hansard for {date}")
+		df_text = df_text[df_text.Card != ''] # Drop empty Card column entries as these will be repeats of Cards below
+		df_text.to_csv(f"output/hansard-{date}.csv", index=False)
+
+		df_stats = df_stats[df_stats.Card != ''] # Drop empty Card column entries as these will be repeats of Cards below
+		df_stats.to_csv(f"output/hansard-{date}-stats.csv", index=False)
+
+		print(f"Found {len(df_text)} records and {len(df_stats)} statistics mentioned in Hansard for {date}")
 		return 'Complete'
 
 
